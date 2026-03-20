01e4a5bbd2f54aa9c3720fa0944ce4ce16198a7d
Tarun Saini <sainitarun7888@gmail.com>
2026-03-20 13:38:50 +0530

    fix- uncomment sendOTPEmail

--- services/auth.services.js
+++ services/auth.services.js
@@ -131,7 +131,7 @@ exports.sendOtp = async (req, res) => {
 
         await otpRepo.saveOTP(email, otp, otpType);
         console.log("reach 1");
-        // await sendOTPEmail(email, otp);
+        await sendOTPEmail(email, otp);
         console.log("reach 2");
 
         return {
