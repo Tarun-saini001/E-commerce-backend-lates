e6c88486c3bd77241ca3ae57f5ffe556ed8b8e3e
Tarun Saini <sainitarun7888@gmail.com>
2026-03-25 16:39:30 +0530

    refactor- transform cart item before order creation

--- services/oerder.services.js
+++ services/oerder.services.js
@@ -4,6 +4,96 @@ const orderRepository = require("../repository/order.repository");
 const userRepository = require("../repository/user.repository")
 const mongoose = require('mongoose');
 
+// exports.createOrder = async (req) => {
+//     console.log('req.body: ', req.body);
+//     const userId = req.user;
+//     const body = req.body;
+
+
+//     const userData = await userRepository.findUserById(userId);
+//     console.log('userData: ', userData);
+
+//     if (!userData) {
+//         return {
+//             status: "RecordNotFound",
+//             message: "User not found",
+//         };
+//     }
+//     // get user cart
+//     const cart = await cartModel.findOne({ user: userId });
+
+//     if (!cart || cart.items.length === 0) {
+//         return {
+//             status: "RecordNotFound",
+//             message: "Cart is empty",
+//         };
+//     }
+
+//     // calculate totals
+//     const subtotal = cart.subtotal;
+//     const shippingFee = 15;
+//     const tax = subtotal * 0.05;
+//     const total = subtotal + shippingFee + tax;
+
+
+//     const orderPayload = {
+//         user: userId,
+
+//         billingDetails: {
+//             name: userData.name,
+//             country: body.billingDetails.country,
+//             city: body.billingDetails.city,
+//             district: body.billingDetails.district,
+//             postalCode: body.billingDetails.postalCode,
+//             address: body.billingDetails.address,
+//             phone: body.billingDetails.phone,
+//             email: userData.email,
+//         },
+
+//         items: cart.items,
+
+//         shippingMethod: body.shippingMethod,
+
+//         subtotal,
+//         shippingFee,
+//         tax,
+//         total,
+//         orderStatus: body.orderStatus
+//     };
+
+
+//     const order = await orderRepository.createOrder(orderPayload);
+//     console.log('order: (create) ', order);
+
+//     const populatedOrder = await order.populate("items.productId");
+
+//     const transformedItems = populatedOrder.items.map((item) => ({
+//         _id: item.productId._id,
+//         title: item.productId.title,
+//         price: item.productId.price,
+//         thumbnail: item.productId.thumbnail,
+//         brand: item.productId.brand,
+//         category: item.productId.category,
+//         quantity: item.quantity,
+//     }));
+
+//     // clear cart after order
+//     await cartModel.findOneAndUpdate(
+//         { user: userId },
+//         { items: [], subtotal: 0 }
+//     );
+
+
+//     return {
+//         status: "Success",
+//         message: "Order placed successfully",
+//         data: {
+//             ...populatedOrder.toObject(),
+//             items: transformedItems
+//         }
+//     };
+// };
+
 exports.createOrder = async (req) => {
     console.log('req.body: ', req.body);
     const userId = req.user;
@@ -36,6 +126,17 @@ exports.createOrder = async (req) => {
     const total = subtotal + shippingFee + tax;
 
 
+    const populatedCartItems = await cart.populate("cart.items.productId");
+
+    const transformedItems = populatedCartItems.items.map((item) => ({
+        _id: item.productId._id,
+        title: item.productId.title,
+        price: item.productId.price,
+        thumbnail: item.productId.thumbnail,
+        brand: item.productId.brand,
+        category: item.productId.category,
+        quantity: item.quantity,
+    }));
     const orderPayload = {
         user: userId,
 
@@ -50,7 +151,7 @@ exports.createOrder = async (req) => {
             email: userData.email,
         },
 
-        items: cart.items,
+        items: transformedItems,
 
         shippingMethod: body.shippingMethod,
 
@@ -65,17 +166,6 @@ exports.createOrder = async (req) => {
     const order = await orderRepository.createOrder(orderPayload);
     console.log('order: (create) ', order);
 
-    const populatedOrder = await order.populate("items.productId");
-
-    const transformedItems = populatedOrder.items.map((item) => ({
-        _id: item.productId._id,
-        title: item.productId.title,
-        price: item.productId.price,
-        thumbnail: item.productId.thumbnail,
-        brand: item.productId.brand,
-        category: item.productId.category,
-        quantity: item.quantity,
-    }));
 
     // clear cart after order
     await cartModel.findOneAndUpdate(
@@ -87,14 +177,9 @@ exports.createOrder = async (req) => {
     return {
         status: "Success",
         message: "Order placed successfully",
-        data: {
-            ...populatedOrder.toObject(),
-            items: transformedItems
-        }
+        data: order
     };
 };
-
-
 
 exports.getOrders = async (req) => {
     const userId = req.user;
@@ -146,7 +231,6 @@ exports.getOrderById = async (req) => {
     }
 
     const order = await orderRepository.findOrderById(orderId).populate("items.productId");
-    console.log('order: ', order);
 
     if (!order || order.user.toString() !== userId.toString()) {
         return {
