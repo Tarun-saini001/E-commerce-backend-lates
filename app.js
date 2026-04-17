fe4d972092097f43d4d9030b7fe20f6d5ab523b5
Tarun Saini <sainitarun7888@gmail.com>
2026-04-17 09:52:21 +0530

    deploy

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(cookieParser())
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(cors({
-    // origin:process.env.ALLOW_ORIGIN,
-    origin: "http://localhost:5173",
+    origin:process.env.ALLOW_ORIGIN,
+    // origin: "http://localhost:5173",
     credentials: true
 }))
 
--- routes/routes.order.js
+++ routes/routes.order.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const validate = require("../middlewares/validateRequest");
 const verifyToken = require("../middlewares/verifyToken");
 const orderController = require("../controllers/order.controller");
-const { orderSchema } = require("../validations/order");
+// const { orderSchema } = require("../validations/order");
 const { ROLES } = require("../config/constants");
 
-router.post("/add",verifyToken(ROLES.USER,ROLES.ADMIN),validate(orderSchema),orderController.createOrder)
+router.post("/add",verifyToken(ROLES.USER,ROLES.ADMIN),orderController.createOrder)
 router.get("/orders",verifyToken(ROLES.ADMIN),orderController.getAllOrders)
 router.get("/", verifyToken(ROLES.USER,ROLES.ADMIN), orderController.getOrders);
 router.get("/:id",verifyToken(ROLES.USER,ROLES.ADMIN),orderController.getOrderById)
